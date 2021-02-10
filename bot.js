9440e7d4cc6c7af28430117e653e43ba7e4bd94c
Embaren <77256554+Embaren@users.noreply.github.com>
2021-02-10 01:32:25 +0100

    add scorem

--- bot.js
+++ bot.js
@@ -5,29 +5,32 @@ const bot=new Discord.Client();
 const cmds=require('./commands.js');
 
 bot.on('message',function(message){
-if (message.content[0]==='€'){
-		content=message.content.slice(1).trim().split(/\s+/)
-		if (typeof content != "undefined" && content != null && content.length != null && content.length > 0 && content[0]!='') {
-			command=content.shift();
-			switch (command){
-				case ('help'):
-					reply=cmds.help();
-					break;
-				case ('ping'):
-					reply=cmds.ping(function(reply){message.channel.send(reply);});
-					break;
-				case ('score'):
-					reply=cmds.score(message.author);
-					break;
-				default:
-					reply="Requête invalide";
+	if (message.content[0]==='€'){
+			content=message.content.slice(1).trim().split(/\s+/)
+			if (typeof content != "undefined" && content != null && content.length != null && content.length > 0 && content[0]!='') {
+				command=content.shift();
+				switch (command){
+					case ('help'):
+						reply=cmds.help(function(reply){message.channel.send(reply);});
+						break;
+					case ('ping'):
+						reply=cmds.ping(function(reply){message.channel.send(reply);});
+						break;
+					case ('score'):
+						reply=cmds.score(message.author,function(reply){message.channel.send(reply);});
+						break;
+					case ('scorem'):
+						reply=cmds.scorec(message.author,content,function(reply){message.channel.send(reply);});
+						break;
+					default:
+						reply="Requête invalide";
+				}
 			}
+			else {
+				reply="Requête invalide";
+			}
+			//message.channel.send(reply);
 		}
-		else {
-			reply="Requête invalide";
-		}
-		//message.channel.send(reply);
-	}
 });
 
 bot.login(process.env.BOT_TOKEN);
--- commands.js
+++ commands.js
@@ -9,12 +9,17 @@ var con = mysql.createPool({
 	database: "ywv3b7ro3jcrn5hz"
 });
 
-
+function isNormalInteger(str) {
+    var n = Math.floor(Number(str));
+    return n !== Infinity && String(n) === str;
+}
 
 module.exports = {
-	help : function(page=1) {return "**Merci d'utiliser le bod Discord des RUDF !**\nLes commandes sont utilisées avec le préfixe '€' :\n - help : Affiche la liste des commandes\n - ping : Répond 'Pong !'"},
+	help : function(callback) {callback("**Merci d'utiliser le bod Discord des RUDF !**\nLes commandes sont utilisées avec le préfixe '€' :\n - help : Affiche la liste des commandes\n - ping : Répond 'Pong !'\n - score : Affiche votre score de citoyenneté\n - scorem : Applique un modificateur à votre score de citoyenneté");},
+	
 	ping : function(callback) {callback('Pong !')},
-	score : function(author) {
+	
+	score : function(author,callback) {
 		con.query('SELECT modifier FROM bot_scores WHERE citizen ="'+author.username+author.discriminator+'" LIMIT 1;', function (err,result){
 			
 			if (err) {
@@ -25,9 +30,39 @@ module.exports = {
 				modifier=result[0].modifier;
 			}
 			console.log(modifier);
-			return("Modifieur : "+modifier);
+			callback("Score : "+modifier);
 		});
-		return("L'accès à votre score de citoyenneté n'a pas pu aboutir.")
+	},
+	
+	scorem : function(author,content,callback) {
+		
+		if(content.length>0 && isNormalInteger(content[0]) ){
+		
+			val=Math.floor(Number(content.shift()))
+			
+			if abs(val)<=100{
+			
+				con.query('SELECT modifier FROM bot_scores WHERE citizen ="'+author.username+author.discriminator+'" LIMIT 1;', function (err,result){
+					
+					if (err) {
+						con.query('INSERT INTO bot_scores (citizen, modifier) VALUES ("'+author.username+author.discriminator+'",'+val+');', function (err2){if (err2) throw err2;});
+					}
+					else {
+						modifier=result[0].modifier+val;
+						con.query('UPDATE bot_scores SET modifier='+modifier+' WHERE citizen="'+author.username+author.discriminator+'";', function (err2){if (err2) throw err2;});
+						
+					}
+					console.log(modifier);
+					callback("Le modificateur a bien été appliqué.");
+				});
+			}
+			else { 
+				callback('Le modificateur de score doit être inclus entre -100 et 100.')
+			}
+		}
+		else{
+			callback('Modificateur de score non reconnu.')
+		}
 	}
 };
 // CREATE USER 'RUDF_bot'@'%' IDENTIFIED BY 'RUDFbot2021';
