ca5c3d53cdc2dabe78e9beec05c5c6729cf32525
Embaren <77256554+Embaren@users.noreply.github.com>
2021-02-11 10:43:00 +0100

    Update commands.js

--- commands.js
+++ commands.js
@@ -68,9 +68,10 @@ module.exports = {
 	
 	citation : function(channels,callback) {
 		 const channel = channels.cache.find(channel => channel.id === "779064607443779594");
-		 message=channel.messages.cache.filter(message => message.content.includes('"')).random(1);
+		 message=channel.messages.cache.filter(message => message.content.includes('"'));
 		 console.log(message);
 		 //callback(message.content);
+		 //.random(1)
 		 return;
 	},
 	
