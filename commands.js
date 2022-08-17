66b2123d7360af49bb5af8b3d1f0af6df887de97
Embaren <e.barbier.renard@gmail.com>
2022-08-17 15:33:31 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -880,8 +880,9 @@ ${correctedContent}`);
 				textEmbed.addField(commandes[i], descriptions[i], false );
 			}
 			relationshipDesc = getRelationshipDesc();
-			for (i=0;i<relationshipDesc.length;i++){
-				textEmbed.addField(relationshipDesc[i], false );
+			textEmbed.addField("Les relations possibles sont:","",false);
+			for (i=0;i<relationshipList.length;i++){
+				textEmbed.addField(i.toString(),relationshipList[i], false );
 			}
 		callback(textEmbed);
 		return;
